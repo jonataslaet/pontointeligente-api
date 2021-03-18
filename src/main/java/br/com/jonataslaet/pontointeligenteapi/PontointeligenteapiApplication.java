87126e45976648f4ff50f6b07b7234a6a117c8be
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-03-17 21:44:14 -0300

    Criado o projeto backend da aplicação também foi incluído o travis yml para integração contínua

---
+++ src/main/java/br/com/jonataslaet/pontointeligenteapi/PontointeligenteapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonataslaet.pontointeligenteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jonataslaet/pontointeligenteapi/PontointeligenteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonataslaet.pontointeligenteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
