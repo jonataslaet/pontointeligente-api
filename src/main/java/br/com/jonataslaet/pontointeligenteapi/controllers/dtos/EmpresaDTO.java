d671f2e2ee80681d04b08ed041c145ab20b7fdcd
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-03-21 10:46:21 -0300

    Criado o dto EmpresaDTO esse DTO é utilizado para buscar de empresa

---
+++ src/main/java/br/com/jonataslaet/pontointeligenteapi/controllers/dtos/EmpresaDTO.java
@@ -0,0 +1,36 @@
+package br.com.jonataslaet.pontointeligenteapi.controllers.dtos;
+
+public class EmpresaDTO {
+
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	
+	public EmpresaDTO() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+}
